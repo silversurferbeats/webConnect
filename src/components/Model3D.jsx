2c6354c46e8591252ea11f3c8a24c4add3253ed1
silversurferbeats <gabrielsuarez89@gmail.com>
2024-04-08 03:00:49 -0300

    fix(Model3D): corrected 3D model reload

---
+++ src/components/Model3D.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Model3D() {
+  return (
+    <Spline scene="https://prod.spline.design/mHwoxMgPCyIB6Ss1/scene.splinecode" />
+  );
+}
