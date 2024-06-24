bb4c42870a7e26d0a3cf8b1c613f0e53daed5837
silversurferbeats <gabrielsuarez89@gmail.com>
2024-06-24 20:59:48 -0300

    feat(Model3D): add new Pet website

--- src/components/Model3D.jsx
+++ src/components/Model3D.jsx
@@ -2,6 +2,6 @@ import Spline from '@splinetool/react-spline';
 
 export default function Model3D() {
   return (
-    <Spline scene="https://prod.spline.design/mHwoxMgPCyIB6Ss1/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/RvJfG3VvA4Fe-JMe/scene.splinecode" />
   );
 }
